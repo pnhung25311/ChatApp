ad0cf0e51add0ce758b81e48f4c744a966951b66
pnhung25311 <pnhung25311@gmail.com>
2025-07-12 14:33:05 +0700

    commit

--- back-end/src/routers/auth.route.js
+++ back-end/src/routers/auth.route.js
@@ -4,9 +4,6 @@ import { login, signup, logout, updateProfile, checkAuth } from "../controllers/
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 router.post("/signup", signup)
-router.get("/", (req,res)=>{
-    res.json({data:"hh"})
-})
 router.post("/login", login)
 router.post("/logout", logout)
 router.put("/update-profile", protectRoute, updateProfile)
